806cb851d2e401ff23925173bd7fde3c07bd9914
Raj Vhora <rajvhora2000@outlook.com>
2021-07-12 12:52:36 +0530

    Changes made for inward supplies

--- GenerateCombinedFiles.py
+++ GenerateCombinedFiles.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import os
 
-Header = ["GSTIN of supplier","Trade/Legal name of the Supplier","Invoice number","Invoice type","Invoice Date","Invoice Value (₹)","Place of supply","Supply Attract Reverse Charge","Rate (%)","Taxable Value (₹)","Integrated Tax  (₹)","Central Tax (₹)","State/UT tax (₹)","Cess  (₹)","Counter Party Return status"]
+Header = ["GSTIN of supplier","Invoice number","Invoice type","Invoice Date","Invoice Value (₹)","Place of supply","Supply Attract Reverse Charge","Rate (%)","Taxable Value","Integrated Tax","Central Tax","State/UT tax","Cess","GSTR-1/5 Filing Status","GSTR-3B Filing Status","Amendment made if any","Tax Period in which Amended","Effective date of cancellation","Source","IRN","IRN Date"]
 dataframes = []
 for files in os.listdir('Excel Files/'):
 
-        dfs = pd.read_excel('Excel Files/'+files,sheet_name = "B2B")
-        temp = pd.DataFrame(dfs[5:])
+        dfs = pd.read_excel('Excel Files/'+files,sheet_name = "MyReport_Taxable_inward_supplie")
+        temp = pd.DataFrame(dfs[3:])
         dataframes.append(temp)
 data = pd.concat(dataframes)
 data.columns = Header
